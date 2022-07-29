4513d1d36a72d2925f84942c6d07beea18e8ed07
ashachakre0906 <chourpagar.asha@gmail.com>
2022-07-29 13:45:11 -0700

    Added data to the models

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,21 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
-
+//Create our location model
 class Category extends Model {}
-
+//Create fields/columns for location model
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
